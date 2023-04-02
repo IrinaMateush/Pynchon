3da28177b5e81fd7f9937a9623769b141c32281d
Irina Mateush <ismateush@gmail.com>
2023-04-02 22:31:18 +0300

    цвета и один файл стилей

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 
-gulp.task('default', function () {
+gulp.task('cssglue', function () {
  return gulp.src('blocks/**/*.css')
    .pipe(concatCss("styles/bundle.css"))
    .pipe(gulp.dest('out/'));
